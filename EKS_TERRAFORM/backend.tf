7769d2d6079cf050cb8f860f361bf5b039220192
dimili2212 <135544813+dimili2212@users.noreply.github.com>
2025-08-04 17:23:00 +0100

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "devsecops-2048-dimili-1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "eu-north-1"
   }
 }
